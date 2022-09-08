b399a751dcf7835e22828c9927f02321714172e1
nkruger <nicolas.nkruger@gmail.com>
2022-09-08 14:30:08 -0300

    add private chat

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,34 @@ import type { NextPage } from 'next'
 import { FormEvent, useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import { io } from "socket.io-client"
+import random from "random-name"
 
-type ToDo = {
-  id: number;
-  text: string;
-  done: boolean;
+type Chat = {
+  reciver: string;
+  sender: string;
+  content: string;
 };
 
 const Home: NextPage = () => {
   
+  const [myName, setMyName] = useState("")
   const [state, setState] = useState<"connect"|"disconect">("disconect");
   const [text, setText] = useState("");
-  const [message, setMessage] = useState<string[]>([]);
+  const [name, setName] = useState("");
+  const [chat, setChat] = useState<Chat[]>([]);
   const [socket, setSocket] = useState(io('ws://localhost:3333'))
-  const [toDos, setTodos] = useState<ToDo[]>([])
-
+  
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault()
     event.stopPropagation()
-    socket.emit("add", text)
+
+    const newChat: Chat = {
+      reciver: name,
+      sender: myName,
+      content: text
+    }
+
+    socket.emit("chat", newChat)
     setText("")
   }
 
@@ -28,35 +37,36 @@ const Home: NextPage = () => {
     socket.on("connect", () => {
       setState("connect")
     })
-    socket.on("update", args => {
-      setTodos(args)
+    socket.on(`chat-${myName}`, args => {
+      setChat([args, ...chat])
     })
 
     return () => {
       socket.off("connect")
-      socket.off("update")
+      socket.off(`chat-${myName}`)
     }
-  }, [toDos])
-
-
-  const handleToggle = (id: number) => {
-    socket.emit("toggle", id)
-  }
-
+  }, [chat, myName])
 
   return (
     <div className={styles.container}>
-      <h1>{state}</h1>
+      <h1>{state}: {myName}</h1>
+      <button onClick={() => setMyName(random.first())}>change name</button>
+      <h2>name</h2>
+      <input value={name} type="text" onChange={e => setName(e.target.value)} />
+      <button>send</button>
+      <h2>text</h2>
      <form onSubmit={handleSubmit} action="">
       <input value={text} type="text" onChange={e => setText(e.target.value)}/>
       <button>send</button>
      </form>
      <ul>
         {
-          toDos.map((todo) => (
-            <li key={todo.id}>
-              {todo.text}
-              <button onClick={() => handleToggle(todo.id)} >{todo.done?"done":"undone"}</button>
+          chat.map((c, i) => (
+            <li key={i}>
+              <strong>
+                {c.sender}:
+              </strong>
+              {c.content}
             </li>
           ))
         }
